32255c2f7e9192d486c2ded2a8d2153343772b6f
Borislove <abirme@yandex.ru>
2021-05-28 15:03:59 +0300

    Update dsfff.java

--- src/dsfff.java
+++ src/dsfff.java
@@ -1,11 +1,8 @@
+// объявляется класс Test
+public class Test{
 
-
-
-public class dsfff {// объявляется класс Test
-
-    public static void main(String[] args) {// так включаются программы,
-        // просто запомнить
-
+    //main - точка входа в программу
+    public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("5 раз");
     }
